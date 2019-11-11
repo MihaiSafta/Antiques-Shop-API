c836fc2d32774886faa7c79dd90fe872bb51c677
Mihai <mihaisaftas.259@gmail.com>
2019-11-11 19:52:25 +0200

    upd

--- src/main/java/org/fasttrackit/onlineshop/transfer/product/AddProductToCartRequest.java
+++ src/main/java/org/fasttrackit/onlineshop/transfer/product/AddProductToCartRequest.java
@@ -5,6 +5,23 @@ public class AddProductToCartRequest {
     private long id;
     private String name;
     private double price;
+    private long customerId;
+    private long productId;
 
 
+    public void setCustomerId(long customerId) {
+        this.customerId = customerId;
+    }
+
+    public long getCustomerId() {
+        return customerId;
+    }
+
+    public void setProductId(long productId) {
+        this.productId = productId;
+    }
+
+    public long getProductId() {
+        return productId;
+    }
 }
